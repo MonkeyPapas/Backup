d0bcb587da7ca1704b7063b672d0132d7771f8d9
Elliot <it@monkeypapas.com>
2025-04-26 00:38:21 -0500

    feat: testing

--- src/common/controllers/backupController.js
+++ src/common/controllers/backupController.js
@@ -1,16 +1,15 @@
 const db = require('../database/mongoConfig'); // Importamos la configuración de MongoDB
 
-// Obtener un shop por su shopCode (el nombre de la colección)
 exports.getShopById = async (req, res) => {
-  const { id } = req.params; // Obtenemos el id del shop desde la URL
+  const { id } = req.params;
   try {
-    const collection = db.collection(id);  // `id` es el shopCode, que será el nombre de la colección
-    const shopData = await collection.find({}).toArray(); 
+    const collection = db.db.collection(id); // ← importante usar .db aquí
+    const shopData = await collection.find({}).toArray();
 
     if (shopData && shopData.length > 0) {
-      return res.status(200).json(shopData);  // Si se encuentran documentos, retornamos los datos
+      return res.status(200).json(shopData);
     } else {
-      return res.status(404).json({ message: `Shop con shopCode ${id} no encontrado` });  
+      return res.status(404).json({ message: `Shop con shopCode ${id} no encontrado` });
     }
   } catch (error) {
     console.error('Error al consultar el backup:', error);
